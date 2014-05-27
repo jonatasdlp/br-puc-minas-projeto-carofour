7e8a02396e18890e3c99c0ec10eb4d5016db8187
Jonatas Santos <jonatas.dlp@gmail.com>
2014-05-27 01:28:30 -0300

    Ajustes no processo de finalizar compra

--- produto/carofour/src/com/pucminas/carofour/controller/FinalizarCompraServletCtrl.java
+++ produto/carofour/src/com/pucminas/carofour/controller/FinalizarCompraServletCtrl.java
@@ -16,53 +16,59 @@ import com.pucminas.carofour.model.Pedido;
 /**
  * @version 0.2
  */
-
 @WebServlet("/finalizar")
 public class FinalizarCompraServletCtrl extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
+
+    private static final long serialVersionUID = 1L;
+
     public FinalizarCompraServletCtrl() {
         super();
     }
 
-	@Override
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		HttpSession session = request.getSession();
-		Pedido pedido = (Pedido)session.getAttribute("pedido");
-		
-		if (pedido == null) {
-			String contextPath = request.getContextPath();
-			response.sendRedirect(response.encodeRedirectURL(contextPath + "/compras") );
-		} else {
-			request.setAttribute("subtotal", pedido.calcularCustoTotal());
-			RequestDispatcher dispatcher = request.getRequestDispatcher("/fechamentoCompra.jsp"); 
-	    	dispatcher.forward(request, response);
-		} 
-	}
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        HttpSession session = request.getSession();
+        Pedido pedido = (Pedido) session.getAttribute("pedido");
 
-	@Override
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		HttpSession session = request.getSession();
-		Pedido pedido = (Pedido)session.getAttribute("pedido");
-		Cliente cliente = new Cliente();
+        if (pedido == null) {
+            String contextPath = request.getContextPath();
+            response.sendRedirect(response.encodeRedirectURL(contextPath + "/compras"));
+        } else {
+            request.setAttribute("subtotal", pedido.calcularCustoTotal());
+            RequestDispatcher dispatcher = request.getRequestDispatcher("/fechamentoCompra.jsp");
+            dispatcher.forward(request, response);
+        }
+    }
 
-		cliente.setNomeCompleto(request.getParameter("nome"));
-		cliente.setDataNascimento(request.getParameter("nascimento"));
-		cliente.setSexo(request.getParameter("sexo").toCharArray()[0]);
-		cliente.setEndereco(request.getParameter("endereco"));
-		cliente.setTelefone(request.getParameter("telefone"));
-		cliente.setEmail(request.getParameter("email"));
-		cliente.setSenha(request.getParameter("senha"));
-		cliente.gravarDados();
-		
-		pedido.setCliente(cliente);
-		pedido.gerarNumeroPedido();
-		request.setAttribute("numero", pedido.getNumero());
-		request.setAttribute("items", pedido.getItems());
-		request.setAttribute("cliente", pedido.getCliente());
-		RequestDispatcher dispatcher = request.getRequestDispatcher("finalizaCompra.jsp");
-		dispatcher.forward(request, response);
-		
-	}
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        HttpSession session = request.getSession();
+        Pedido pedido = (Pedido) session.getAttribute("pedido");
+        Cliente cliente = new Cliente();
+
+        cliente.setNomeCompleto(request.getParameter("nome"));
+        cliente.setDataNascimento(request.getParameter("nascimento"));
+        cliente.setSexo(request.getParameter("sexo").toCharArray()[0]);
+        cliente.setEndereco(request.getParameter("endereco"));
+        cliente.setTelefone(request.getParameter("telefone"));
+        cliente.setEmail(request.getParameter("email"));
+        cliente.setSenha(request.getParameter("senha"));
+        
+        pedido.setTipoPagamento(request.getParameter("tipoPagamento"));
+        pedido.setCliente(cliente);
+        pedido.gerarNumeroPedido();
+
+        cliente.gravarDados();
+        pedido.gravarDados();
+        session.invalidate();
+
+        request.setAttribute("numero", pedido.getNumero());
+        request.setAttribute("items", pedido.getItems());
+        request.setAttribute("cliente", pedido.getCliente());
+        request.setAttribute("tipoPagamento", pedido.getTipoPagamento());
+        RequestDispatcher dispatcher = request.getRequestDispatcher("finalizaCompra.jsp");
+        dispatcher.forward(request, response);
+
+    }
 
 }
